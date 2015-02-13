239a23f3561a3cd79f93875c46b34393e202247f
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-02-13 10:02:41 -0800

    add spec and 'if' statement to return 'isosceles' if two sides are equal

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 var triangles = (function(side1, side2, side3) {
   if (side1 === side2 && side2 === side3) {
     return 'equalateral';
-  } else {
-    return false;
+  } else if (side1 === side2 || side1 === side3 || side2 === side3) {
+    return 'isosceles';
+  } else if (side1 != side2 && side2 != side3) {
+    return 'scalene';
   }
 });
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe('triangles', function() {
   it('will return equalateral if all sides are equal', function() {
     expect(triangles(2, 2, 2)).to.equal('equalateral');
   });
+
+  it('will return "isosceles" if only two sides are equal', function() {
+    expect(triangles(2, 3, 3)).to.equal('isosceles');
+  });
 });
