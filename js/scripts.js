b9cbc54d7f53a7ed3a17b615c9c802d49b5edf47
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-02-13 17:33:57 -0600

    html form

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,17 @@ var triangles = (function(side1, side2, side3) {
     return 'You do not have a triangle';
   }
 });
+
+$(function() {
+  $('form#triangles').submit(function(event) {
+    var side1 = parseInt($('input#side1').val());
+    var side2 = parseInt($('input#side2').val());
+    var side3 = parseInt($('input#side3').val());
+
+    var result = triangles(side1, side2, side3);
+
+    $('#result').text(result)
+
+    event.preventDefault();
+  });
+});
