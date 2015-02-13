d6674a1d47f28b45f16718ae9c9d73810a3c9a7a
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-02-13 10:54:37 -0800

    add functionality to determine if input values can be a triangle

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 var triangles = (function(side1, side2, side3) {
-  if (side1 === side2 && side2 === side3) {
-    return 'equalateral';
-  } else if (side1 === side2 || side1 === side3 || side2 === side3) {
-    return 'isosceles';
-  } else if (side1 != side2 && side2 != side3) {
-    return 'scalene';
+  if ((side1 + side2) >= side3 && (side1 + side3) >= side2 && (side2 + side3) >= side1) {
+    if (side1 === side2 && side2 === side3) {
+      return 'equalateral';
+    } else if (side1 === side2 || side1 === side3 || side2 === side3) {
+      return 'isosceles';
+    } else if (side1 != side2 && side2 != side3) {
+      return 'scalene';
+    }
+  } else {
+    return 'You do not have a triangle';
   }
 });
--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,12 @@ describe('triangles', function() {
   it('will return "isosceles" if only two sides are equal', function() {
     expect(triangles(2, 3, 3)).to.equal('isosceles');
   });
+
+  it('will return "scalene" if no sides are equal', function() {
+    expect(triangles(1, 2, 3)).to.equal('scalene');
+  });
+
+  it('will return "You do not have a triangle" if one side is not as long as the other two combined', function() {
+    expect(triangles(2, 2, 8)).to.equal('You do not have a triangle');
+  });
 });
