63385c1f91f371eef295e00072cae226258ec7ab
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-02-13 09:56:38 -0800

    add spec and 'if' statement to return 'equilateral'

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 var triangles = (function(side1, side2, side3) {
-  
+  if (side1 === side2 && side2 === side3) {
+    return 'equalateral';
+  } else {
+    return false;
+  }
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,3 +1,5 @@
 describe('triangles', function() {
-  
+  it('will return equalateral if all sides are equal', function() {
+    expect(triangles(2, 2, 2)).to.equal('equalateral');
+  });
 });
